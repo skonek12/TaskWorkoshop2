060affd6e1fb0e1b7f1b0a3af4fe9aac4fc70f2c
Lukasz <lukasz.skoniecki1@gmail.com>
2022-12-01 23:06:46 +0100

    pierwszy commit

---
+++ TaskWorkshop/src/main/java/pl/coderslab/entity/User.java
@@ -0,0 +1,25 @@
+package pl.coderslab.entity;
+
+public class User {
+
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
